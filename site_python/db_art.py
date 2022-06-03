9f6cd953436010fc5194c342dc52471624bf6bc2
Rivoconcept <rivo.hanitrarivelo@gmail.com>
2022-06-03 11:20:42 +0300

    avancement du projet

---
+++ site_python/db_art.py
@@ -0,0 +1,17 @@
+arts = [
+    {
+        "Titre":"Article n°1", 
+        "Date_de_publication":"02/06/2022",
+        "Contenu":"Contenu du premier article"
+    }, 
+      {
+        "Titre":"Article n°2", 
+        "Date_de_publication":"02/06/2022",
+        "Contenu":"Contenu du deuxième article"
+    }, 
+      {
+        "Titre":"Article n°3", 
+        "Date_de_publication":"02/06/2022",
+        "Contenu":"Contenu du troisième article"
+    },
+]
--- site_python/views.py
+++ site_python/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 
+from django.shortcuts import render
+from .db_art import arts
+
+    
 def accueil(request):
     return render(request, "accueil.html")
 
 def articles(request):
-    return render(request, "articles.html")
+    return render(request, "articles.html", context = {'arts':arts})
 
 def contact(request):
     return render(request, "contact.html")
